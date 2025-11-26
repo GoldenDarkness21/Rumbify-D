e03ff7f3163784463474aa9d9364e9cd1dddd197
Brandon <martinezbran0221@gmail.com>
2025-11-26 02:04:07 -0500

    Fix profile image upload endpoint to use backend URL

--- app1/screens/editProfile.js
+++ app1/screens/editProfile.js
@@ -571,7 +571,8 @@ async function uploadProfileImage(file) {
   formData.append('profile_image', file);
   
   // Upload image to server with user email in header
-  const response = await fetch(`/users/${currentUser.id}/profile-image`, {
+  const BASE_URL = "https://my-backend-weld-beta.vercel.app";
+  const response = await fetch(`${BASE_URL}/users/${currentUser.id}/profile-image`, {
     method: 'POST',
     headers: {
       'x-user-email': currentUser.email
