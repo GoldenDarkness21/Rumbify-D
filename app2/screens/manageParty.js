3860b744b3e767a181a32ec43c9fe2d2339c1de1
Brandon <martinezbran0221@gmail.com>
2025-11-26 03:21:07 -0500

    Fix corrupted close button characters in modals

--- app2/screens/manageParty.js
+++ app2/screens/manageParty.js
@@ -58,8 +58,8 @@ export default async function renderManageParty(routeData = {}) {
           <img id="ga-avatar" src="/assets/userIcon.svg" alt="Avatar" class="modal-avatar"/>
           <div id="ga-name" class="modal-name">Invitado</div>
           <div class="modal-actions">
-            <button id="ga-approve" class="btn btn-approve">✁E/button>
-            <button id="ga-reject" class="btn btn-reject">X</button>
+            <button id="ga-approve" class="btn btn-approve">✓</button>
+            <button id="ga-reject" class="btn btn-reject">✗</button>
           </div>
         </div>
       </div>
@@ -87,7 +87,7 @@ export default async function renderManageParty(routeData = {}) {
         <div class="modal-content">
           <div class="modal-header">
             <h3>Create Entry Codes</h3>
-            <button id="closeCreateModal" class="close-modal">ÁE/button>
+            <button id="closeCreateModal" class="close-modal">×</button>
           </div>
           <form id="createCodesForm" class="create-codes-form">
             <div class="form-group">
@@ -113,7 +113,7 @@ export default async function renderManageParty(routeData = {}) {
         <div class="modal-content">
           <div class="modal-header">
             <h3>Generated Codes</h3>
-            <button id="closeDisplayModal" class="close-modal">ÁE/button>
+            <button id="closeDisplayModal" class="close-modal">×</button>
           </div>
           <div class="codes-display">
             <p><strong>Ticket Type:</strong> <span id="displayTicketType"></span></p>
